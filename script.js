1b8e572f3be0fae4bf40bcb6c41b2f78a4c41f23
Zachary Amiton <zamiton@outlook.com>
2020-06-22 02:33:38 -0700

    the game is afoot
    - no function yet but it does look like it might work

--- script.js
+++ script.js
@@ -3,27 +3,60 @@ const DONE = 4
 const OK = 200
 const NOT_FOUND = 404
 
-var testButton = document.getElementById("test_button")
-testButton.addEventListener('click', function() {
-    startApp()
+var show = (el, val='') => el.style.display = (val) ? (val) : ("block")
+var hide = (el, actualHide=false) => el.style.display = (!actualHide) ? ('none') : ('hidden')
+
+var formDiv = document.getElementById("intro")
+var albumForm = document.getElementById("album-form")
+var loadingAnimation = document.getElementById("loading")
+
+var bracketDiv = document.getElementById('bracket')
+var songList = document.getElementById('song-list')
+
+var albumTracks = []
+
+
+Document.prototype.createElementWithText = function(tagName, text) {
+    let elem = this.createElement(tagName)
+    elem.textContent = text
+    return elem
+}
+
+albumForm.addEventListener('submit', function() {
+    let [artist, album] = [albumForm.elements.artist.value, albumForm.elements.album.value]
+    
+    startApp(artist, album)
 })
 
-
-async function getAlbumTracks(artist="Feed Me Jack", album="Chumpfrey") {
-    album = geniusClean(album)
-    artist = geniusClean(artist)
-
-    //probably shouldn't use cors-anywhere but idk why it ain't working from ghd
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://genius.com/albums/${artist}/${album}`)
+async function getAlbumTracks(artist, album) {
+    //probably shouldn't use cors-anywhere but idk why it ain't working from ghp
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://genius.com/albums/${geniusClean(artist)}/${geniusClean(album)}`)
+    
     const pageContent = new DOMParser().parseFromString(await response.text(), 'text/html')
     let tracks = Array.from(pageContent.getElementsByClassName("chart_row-content-title")).map(t => t.textContent.trim().slice(0, -1*("Lyrics").length).trim())
     return tracks
 }
 
-async function startApp() {
-    let tracks = await getAlbumTracks()
-    console.log(tracks)
+
+async function startApp(artist, album) {
+    if(artist && album) {
+        show(loading)
+        for(let an of document.getElementsByClassName("album-name")) an.textContent = album
+        albumTracks = await getAlbumTracks(artist, album)   
+        if(albumTracks.length > 0) {
+            document.getElementById("battle-tracks").textContent = albumTracks.join(", ")
+            hide(formDiv)
+            show(bracketDiv)
+        } else {
+            hide(loading)
+            alert(`Album ${album} by ${artist} not found on Genius!`)
+        }
+    }
+    
+    //tracks.forEach(t => songList.appendChild(document.createElementWithText("li", t)))
 }
+    
+   
   
 function geniusClean(fi) {
     let splitSet = ["ft.", "feat.", "featuring.", "(with"]
