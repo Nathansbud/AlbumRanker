140e93010c3beced726e899bab18ec0772a22532
Zachary Amiton <zamiton@outlook.com>
2020-06-23 04:00:30 -0700

    the functionality* baby
    *there is a massive bug rn i am too tired to fix will do tomorrow

--- script.js
+++ script.js
@@ -11,9 +11,17 @@ var albumForm = document.getElementById("album-form")
 var loadingAnimation = document.getElementById("loading")
 
 var bracketDiv = document.getElementById('bracket')
-var songList = document.getElementById('song-list')
+var leftOption = document.getElementById("battle-left")
+var rightOption = document.getElementById("battle-right")
+var lr = ["battle-left", "battle-right"]
+
+var resultsDiv = document.getElementById('results')
+var resultsTable = document.getElementById('results-table')
 
 var albumTracks = []
+var albumRanking = []
+var processSet = []
+var processedIndex = 0
 
 
 Document.prototype.createElementWithText = function(tagName, text) {
@@ -28,6 +36,80 @@ albumForm.addEventListener('submit', function() {
     startApp(artist, album)
 })
 
+Array.from(document.getElementsByClassName('battle-option')).forEach(bo => bo.addEventListener('click', function() {
+    processBattle(processSet, bo.textContent, document.getElementById((lr.indexOf(bo.getAttribute('id')) == 0) ? (lr[1]) : (lr[0])).textContent)
+}))
+
+function processBattle(cArr, winner, loser) {
+    console.log({winner: winner, loser: loser})
+    console.log(cArr, processSet)
+    if(albumRanking.length < albumTracks.length) {
+        if(cArr.length > 1) {
+            if(cArr.includes(loser)) processSet = cArr.slice(cArr.indexOf(loser)+1)
+            else processSet = cArr.slice(0, cArr.indexOf(winner))
+            
+            setBattle(processSet[Math.floor(processSet.length / 2)], (cArr.includes(loser)) ? (winner) : (loser))
+        } else if(cArr.length == 1) {
+            if(cArr.includes(loser)) albumRanking.splice(cArr.indexOf(loser) + 1, 0, winner)
+            else albumRanking.splice(cArr.indexOf(winner) - 1, 0, loser)
+            
+            processSet = []        
+            setBattle(albumRanking[Math.floor(albumRanking.length / 2)], albumTracks[++processedIndex])
+        } else {
+            if(albumRanking.length == 0) {
+                albumRanking.push(loser, winner)
+                processedIndex += 2
+                setBattle(albumRanking[1], albumTracks[processedIndex])
+            } else if(albumRanking.length == 2) {
+                if(albumRanking.includes(winner)) {
+                    albumRanking.splice(1, 0, loser)
+                } else {
+                    albumRanking.push(winner)
+                }
+                setBattle(albumRanking[1], albumTracks[++processedIndex])
+            } else {
+                //there's a bug right now to do with out of bounds but i'm literally so tired i can't keep my eyes open
+                if(processSet.length == 0) {
+                    if(albumRanking.includes(winner)) {
+                        processSet = albumRanking.slice(0, albumRanking.indexOf(winner))
+                    } else {
+                        console.log(albumRanking.indexOf(loser))
+                        processSet = albumRanking.slice(albumRanking.indexOf(loser) + 1)
+                    }
+                } else {
+                    if(processSet.includes(winner)) {
+                        processSet = processSet.slice(0, processSet.indexOf(winner))
+                    } else {
+                        processSet = processSet.slice(processSet.indexOf(loser) + 1)
+                    }
+                }
+                setBattle(processSet[Math.floor(processSet.length / 2)], albumTracks[processedIndex])
+            }
+        }
+        console.log(albumRanking)
+    } 
+    
+    if(albumRanking.length == albumTracks.length) {
+        function createSongRow(st, i) {
+            let tr = document.createElement("tr")
+            
+            let trackRank = document.createElement("td")
+            trackRank.textContent = i
+            let trackName = document.createElement("td")
+            trackName.textContent = st
+            tr.appendChild(trackRank)
+            tr.appendChild(trackName)
+            return tr
+        }
+
+        setBattle("--", "--")
+        albumRanking.forEach((st, idx) => resultsTable.appendChild(createSongRow(st, idx+1)))
+        hide(bracketDiv)
+        show(resultsDiv)
+    }
+}
+
+
 async function getAlbumTracks(artist, album) {
     //probably shouldn't use cors-anywhere but idk why it ain't working from ghp
     const response = await fetch(`https://cors-anywhere.herokuapp.com/https://genius.com/albums/${geniusClean(artist)}/${geniusClean(album)}`)
@@ -35,6 +117,11 @@ async function getAlbumTracks(artist, album) {
     const pageContent = new DOMParser().parseFromString(await response.text(), 'text/html')
     let tracks = Array.from(pageContent.getElementsByClassName("chart_row-content-title")).map(t => t.textContent.trim().slice(0, -1*("Lyrics").length).trim())
     return tracks
+}
+
+function setBattle(left, right) {
+    leftOption.textContent = left
+    rightOption.textContent = right
 }
 
 
@@ -45,6 +132,10 @@ async function startApp(artist, album) {
         albumTracks = await getAlbumTracks(artist, album)   
         if(albumTracks.length > 0) {
             document.getElementById("battle-tracks").textContent = albumTracks.join(", ")
+            albumTracks = shuffle(albumTracks) //shuffle so things are kept spicy
+            
+            setBattle(albumTracks[0], albumTracks[1])
+            
             hide(formDiv)
             show(bracketDiv)
         } else {
@@ -52,11 +143,7 @@ async function startApp(artist, album) {
             alert(`Album ${album} by ${artist} not found on Genius!`)
         }
     }
-    
-    //tracks.forEach(t => songList.appendChild(document.createElementWithText("li", t)))
-}
-    
-   
+} 
   
 function geniusClean(fi) {
     let splitSet = ["ft.", "feat.", "featuring.", "(with"]
@@ -73,7 +160,16 @@ function geniusClean(fi) {
     return fi.charAt(0).toUpperCase() + fi.slice(1).toLowerCase()
 }
 
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(arr) {
+    let currentIndex = arr.length, randIndex, temp
+    while(0 !== currentIndex) {
+        randIndex = Math.floor(Math.random()*currentIndex)
+        currentIndex -= 1
 
-
-
-
+        temp = arr[randIndex]
+        arr[randIndex] = arr[currentIndex]
+        arr[currentIndex] = temp
+    }
+    return arr
+}
